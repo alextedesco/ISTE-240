d64722e402a9d13b70a717fd60e4f6ee30ec17c8
Alex Tedesco <act2076@rit.edu>
2023-03-20 16:04:06 -0400

    March20

--- Classwork/Mar20/modularSiteStart/assets/inc/nav.php
+++ Classwork/Mar20/modularSiteStart/assets/inc/nav.php
@@ -7,7 +7,7 @@
 					<ol class="nav nav-list container-fluid nav-collapse collapse in">
 						<li <?php if ($title == "Home")(echo 'class="active"';) ?>><a href="index.php">Home</a></li>
 						<li><?php if ($title == "People")(echo 'class="active"';) ?>><a href="people/index.php">Our People</a></li>
-						<li><?php if ($title == "Home")(echo 'class="active"';) ?>><a href="tour/index.php">Tour</a></li>
+						<li><?php if ($title == "Tour")(echo 'class="active"';) ?>><a href="tour/index.php">Tour</a></li>
 						<li><?php if ($title == "TEST")(echo 'class="active"';) ?>><a href="tour/index.php">Test</a></li>
 					</ol>
 				</nav>
