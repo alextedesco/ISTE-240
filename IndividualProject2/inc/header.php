c0da4ad951b7eb14e669ccd39ffb7e3efd53c0a4
Alex <alextedesco8@gmail.com>
2023-04-30 12:22:54 -0400

    Individual Project 2

--- IndividualProject2/inc/header.php
+++ IndividualProject2/inc/header.php
@@ -1,3 +1,7 @@
+<?php
+ $curr = getcwd();
+ echo $curr;
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 
--- IndividualProject2/sights/sights.php
+++ IndividualProject2/sights/sights.php
@@ -2,17 +2,24 @@
 <!-- ISTE-240 -->
 <!-- Individual Project 1 -->
 
-<?php
-    include ("../inc/header.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <!-- Sets the favicon to the Gatlinburg logo -->
+    <link rel="icon" type="image/x-icon" href="../assets/gatlinburg-logo.png">
+    <title>Sights</title>
 </head>
 
 <body>
     <!-- Container for nav, iframe, and form -->
     <div class="container">
         <?php
-          include ("../inc/nav.php");
+            include ("../inc/nav.php");
         ?>
 
         <span>
